8d974aa2e661a05a226eded76cdfc360cab23c45
SmallSun697 <smallsun698@gmail.com>
2024-06-20 21:49:39 +0800

    add footer.html, header.html, day1~day4.html

--- script.js
+++ script.js
@@ -38,6 +38,18 @@ window.addEventListener("scroll", function() {
     CardOpacity(4);
 });
 
+fetch("header.html")
+    .then(response => response.text())
+    .then(data => {
+        document.querySelector("header").innerHTML = data;
+    })
+
+fetch("footer.html")
+    .then(response => response.text())
+    .then(data => {
+        document.querySelector("footer").innerHTML = data;
+    })
+
 function CardOpacity (number) {
     var cardNumber = document.getElementById("card" + number);
     if (!number) return;
@@ -49,5 +61,5 @@ function CardOpacity (number) {
 }
 
 function redirect (url) {
-    window.location.href = url;
+    window.location.href = window.location.href + url;
 }
